ea8e672845d1ad9e5230bc37c86358ad9dc5e691
atparodi10 <joatorres@uamv.edu.ni>
2026-09-02 12:11:01 -0600

    feat: se importa contenido de clase Artesania desde rama de Zahid

--- src/main/java/ni/edu/uam/pae_eventos_javafx/model/Artesania.java
+++ src/main/java/ni/edu/uam/pae_eventos_javafx/model/Artesania.java
@@ -1,4 +1,21 @@
 package ni.edu.uam.pae_eventos_javafx.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+
 public class Artesania {
+    private String nombreProducto;
+    private String productoID;
+    private String productoCategoria;
+    private double precio;
+    private int cantidadDisponible;
+    private String rutaImagen;
 }
+
